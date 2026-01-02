795e6007ba1f16cdbbe08668de925e5da99fb32f
Bhavik Patel <bhavikpate2010@gmail.com>
2026-01-02 16:21:21 +0530

    Fxi the cloud

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(),tailwindcss()],
+    server: {
+        allowedHosts: ['b2b.dynamicdreamz.com'],
+    },
     define: {
         'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
     },
